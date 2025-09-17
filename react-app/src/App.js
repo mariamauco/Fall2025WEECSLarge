f18df611009b313520e23e9826dc49aa8eb20417
Jenna Curls <98861047+jbug16@users.noreply.github.com>
2025-09-17 13:19:06 -0400

    Set up pages with basic routing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Detection from './pages/Detection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <nav>
+          <Link to="/">Dashboard</Link> |{" "}
+          <Link to="/login">Login</Link> |{" "}
+          <Link to="/detection">Detection</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/detection" element={<Detection />} />
+        </Routes>
+      </Router>
   );
 }
 
