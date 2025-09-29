ec4e5a8cdae84979be8916935a0cf504490fc462
mariamauco <mariafmauco@gmail.com>
2025-09-29 16:52:03 -0400

    Server fixes

--- express-backend/models/user.js
+++ express-backend/models/user.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
         username: {type: String, required: true, unique: true},
         email: {type: String, required: true, unique:true},
         password: {type: String, required: true},
-        joinDate: {type: Date, default: Date.now},
+        // joinDate: {type: Date, default: Date.now} automatically added as createdAt
         points: {type: Number, default: 0}
         //consider others: numberScans,etc 
         // 
