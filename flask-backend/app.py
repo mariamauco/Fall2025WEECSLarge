aa9df1d61596836debd152553ba15acae22da596
mariamauco <mariafmauco@gmail.com>
2025-10-27 11:54:32 -0400

    connect model to filepath

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -4,9 +4,10 @@ from ultralytics import YOLO
 app = Flask(__name__)
 
 # load model
-model = YOLO("")
+
 
 filepath = 'best.pt'
+model = YOLO(filepath)
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(host='0.0.0.0', port=5001)
