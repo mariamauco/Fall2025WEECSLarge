6b4a01c62b0bbd327077324322808ee5206e1aea
nic-now <78624899+nic-now@users.noreply.github.com>
2025-10-28 10:55:12 -0400

    Update user schema with detection

--- express-backend/models/user.js
+++ express-backend/models/user.js
@@ -8,9 +8,9 @@ const UserSchema = new mongoose.Schema(
         email: {type: String, required: true, unique:true},
         password: {type: String, required: true},
         //joinDate: {type: Date, default: Date.now}, automatically added as createdAt
-        points: {type: Number, default: 0}
+        points: {type: Number, default: 0},
         //consider others: numberScans,etc 
-        // 
+        detectionsCount: {type: Number, default: 0 } //user's detections count
         
     }, {timestamps: true} //automatically saves when doc was created}   
 )
