e6590a6ee4f2740679414426b497c009e0cf9c1f
mariamauco <mariafmauco@gmail.com>
2025-10-30 13:50:50 -0400

    add name to stats

--- express-backend/routes/userStats.js
+++ express-backend/routes/userStats.js
@@ -12,7 +12,8 @@ router.get('/stats/:userId', async (req, res) => {
       return res.status(400).json({ error: 'User not found' });
     }
 
-    res.status(200).json({username: user.username, 
+    res.status(200).json({name: user.name,
+                          username: user.username, 
                           points: user.points,
                           recycled: user.detectionsCount || 0
                           }); //succesful
@@ -24,6 +25,27 @@ router.get('/stats/:userId', async (req, res) => {
 //GET /api/history: return all detections for user from detections collection in MongoDB 
 router.get('/history/:userId', async (req, res) => {
   //TODO
+  try {
+    const userId = req.params.userId;
+
+    //check if user exists, returns error if not
+    const userExists = await UserData.findById(userId);
+    if (!userExists) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    //finds detections linked to current user
+    //sorts by newest detections to oldest
+    const detections = await Detection.find({ userID: userId }).sort({ createdAt: -1 });
+
+    //returns json of detections; json will be empty if no detections
+    res.status(200).json(detections);
+
+  } catch (err) {
+    console.error('Error fetching user detection history:', err);
+    res.status(500).json({ error: 'Error fetching user detection history' });
+  }
+
 });
 
 module.exports = router;
